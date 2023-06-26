1f77b50c2db97764a8f1f2a01d5669b6e851c636
biliaminu jimoh <favouredbilly@gmail.com>
2023-06-27 00:27:04 +0300

    women program1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 const TravelApp = () => {
-  const [destination, setDestination] = useState('');
+  const [destination, setDestination] = useState("");
 
   const handleDestinationSelect = (selectedDestination) => {
     setDestination(selectedDestination);
@@ -15,45 +15,46 @@ const TravelApp = () => {
         <h2 className="subtitle">Choose your arrival port:</h2>
         <div className="buttons">
           <button
-            className={`destination-button ${destination === 'Tikkurila' ? 'active' : ''}`}
-            onClick={() => handleDestinationSelect('Tikkurila')}
+            className={`destination-button ${
+              destination === "Tikkurila" ? "active" : ""
+            }`}
+            onClick={() => handleDestinationSelect("Tikkurila")}
           >
             Tikkurila
           </button>
           <button
-            className={`destination-button ${destination === 'Kamppi' ? 'active' : ''}`}
-            onClick={() => handleDestinationSelect('Kamppi')}
+            className={`destination-button ${
+              destination === "Kamppi" ? "active" : ""
+            }`}
+            onClick={() => handleDestinationSelect("Kamppi")}
           >
             Kamppi
           </button>
         </div>
       </div>
-      {destination === 'Tikkurila' && (
+      {destination === "Tikkurila" && (
         <div className="destination-content">
           <h2 className="subtitle">Welcome to Tikkurila!</h2>
           <p>follow these steps:</p>
           <div className="destination-content1">
             <ol>
-              <li>Take Bus 570 from Tikkurila. Buses arrive approximately every 10 minutes.</li>
+              <li>
+                Take Bus 570 from Tikkurila. Buses arrive approximately every 10
+                minutes.
+              </li>
               <li>Get off at Silvastintie Bus stop.</li>
               <li>
-                Follow the directions provided in the video below to reach Skyline Airport Hotel:
+                Follow the directions provided in the video below to reach
+                Skyline Airport Hotel:
               </li>
             </ol>
             <div className="video-container">
-              <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/rAz-fy5fs6E"
-                title="YouTube video player"
-                frameBorder="0"
-                allowFullScreen
-              ></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/NTLk_us1wUE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
           </div>
         </div>
       )}
-      {destination === 'Kamppi' && (
+      {destination === "Kamppi" && (
         <div className="destination-content">
           <h2 className="subtitle">Welcome to Kamppi!</h2>
           <p>follow these steps:</p>
@@ -63,18 +64,12 @@ const TravelApp = () => {
               <li>Take Bus 600 at stand 7</li>
               <li>Get off at Silvastintie Bus stop.</li>
               <li>
-                Follow the directions provided in the video below to reach Skyline Airport Hotel:
+                Follow the directions provided in the video below to reach
+                Skyline Airport Hotel:
               </li>
             </ol>
             <div className="video-container">
-              <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/rAz-fy5fs6E"
-                title="YouTube video player"
-                frameBorder="0"
-                allowFullScreen
-              ></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/NTLk_us1wUE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
           </div>
         </div>
